52a4412552cb75ca4cdcad51a0aa60266a49fc01
Paul Halliday <hello@paulhalliday.io>
2018-02-09 20:25:57 +0000

    Added a Node example to diversify implementations

---
+++ slash_integration/node/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  }),
+);
+
+app.post('/appear', (req, res) => {
+  console.log(req);
+  const body = req.body;
+  const roomName = body.command_argument;
+
+  const appearUrl = `📹 [Join my meeting at appear.in!](https://appear.in/${roomName})`;
+
+  res.send(appearUrl);
+});
+
+app.listen(process.env.PORT || 3000, () =>
+  console.log(`Server listening on port ${process.env.PORT || 3000}`),
+);
