81e36545edb20ecd2adcdd8ae2ebf0b0f85561bf
Scott <scott@doist.com>
2020-03-24 12:41:44 +0000

    Adds Python sample for Long Running Task

---
+++ long_running_task_sample/python/readme.md
@@ -0,0 +1,52 @@
+# Long Running Task Python Sample
+
+This is a code sample demonstrating how to perform a long running task when dealing with bots in Twist.
+
+To run this sample we will need to setup and configure a few things:
+- ngrok to forward the Twist calls to your local server
+- An integration on Twist
+- A Flask server to run the server code
+
+## Setup ngrok
+
+In order for your integration to work, Twist needs to know where to send the command/bot data, and for this, you will need a running web server. Whilst you're developing your integration, you may not have a dedicated web server setup for this, and you just want to point to your local machine, this is where [ngrok](https://ngrok.com/) comes in as it can forward the call to your local server. Head to the [ngrok download page](https://ngrok.com/download) to get that installed.
+
+Once installed, you can simply call
+```
+ngrok http 5000
+```
+
+This will start a session that points to port 5000.
+![](../images/ngrok.jpg)
+
+Make a note of the second forwarding URL (in the example, https://4ecc43da.ngrok.io) as we will need this in the next step.
+
+## Setup your integration
+
+To create your integration, go to https://twist.com/integrations/build and click the `Add a new integration` button.
+
+Once you've given it a name and a description, press `Create my integration`; for this sample we will mainly look at Bots. If you go to the Bot section, enter the details of your bot like below
+
+![](../images/bot_config.jpg)
+
+It's here you need that ngrok URL from earlier, enter that into the Request URL with `/bot` at the end as that's the endpoint we are using in the code. Your integration may be different.
+
+Once all that is done and saved, click on `Installation`, make sure you are in the right Twist workspace, and click `Install integration`.
+
+## Setup Flask to run as server
+
+Now, before your integration will work, you need to have that server up and running. 
+
+We're using Flask in this example, but you can use any other setup you're more familiar with. 
+
+Make sure you have Flask installed
+```
+pip install flask
+```
+
+Once you're in the `server.py` directory, it's a simple case of running
+```
+FLASK_APP=server.py flask run
+```
+
+With that complete, you can now go to a conversation with the bot and type something like "hello bot" and it will reply to you.
---
+++ long_running_task_sample/python/server.py
@@ -0,0 +1,78 @@
+from flask import Flask, request, Response
+import requests
+import time
+from datetime import datetime
+from threading import Thread
+
+app = Flask(__name__)
+app.debug = True
+
+
+@app.route("/bot", methods=["POST"])
+def message_received():
+    request_data = request.form
+
+    event_type = request_data.get("event_type")
+
+    # It's good practice with Twist integrations to support being
+    # pinged, it's a good way to test that your integration is
+    # successfully talking to twist. This ping is done from the bot
+    # section of the integration's configuration
+    if event_type == "ping":
+        return Response("pong")
+
+    print(request_data)
+
+    # Here we pass the processing of the message off to a background thread
+    # We do this so that we can immediately respond to the message to
+    # acknowledge we've received it
+    thr = Thread(target=process_bot_conversation, args=[request_data])
+    thr.start()
+
+    # This tells the server we've received the message ok
+    return "", 202
+
+
+def process_bot_conversation(form_data):
+    url_callback = form_data.get("url_callback")
+    url_ttl = form_data.get("url_ttl")
+
+    # We need to check whether the callback url has timed out, we
+    # give you 30 minutes in order to send your message, after which
+    # the callback url will have expired
+    if url_has_timed_out(url_ttl):
+        return
+
+    message = create_message_response(form_data)
+
+    send_reply(url_callback, message)
+
+
+def create_message_response(form_data):
+    """This method is the crux of the bot in terms of determining the content
+    that will be returned to the user"""
+
+    current_content = form_data.get("content")
+    message = "I didn't understand that please type 'help' to see how to use this bot"
+
+    if current_content.startswith("hello") or current_content.startswith("hi"):
+        user_name = form_data.get("user_name")
+        message = u"Hello %s!" % (user_name)
+        time.sleep(5)
+
+    elif current_content == "help":
+        message = "This sample allows you to say 'hi' or 'hello' to the bot"
+
+    return message
+
+
+def send_reply(url_callback, message):
+    payload = {"content": message}
+    requests.post(url_callback, data=payload)
+
+
+def url_has_timed_out(url_ttl):
+    ttl_datetime = datetime.fromtimestamp(float(url_ttl))
+    now = datetime.now()
+
+    return now > ttl_datetime
